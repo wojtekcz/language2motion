5498c7092a2c0cfa625fd2177fca1f8723e3263d
Wojciech Czarnowski <wcz@jatar.com.pl>
2020-04-18 19:08:17 +0200

    MotionDataset: loading set of motions and visualizing them

--- code/Sources/MotionDataset/MotionDataset.swift
+++ code/Sources/MotionDataset/MotionDataset.swift
@@ -1,10 +1,26 @@
 import Foundation
 
 public class MotionDataset {
-    let count: Int
+    let datasetFolderURL: URL
+    var motionSamples: [MotionSample] = []
 
-    public init(a_count: Int) {
-        self.count = a_count
+    public init(datasetFolderURL: URL, maxSamples: Int) {
+        self.datasetFolderURL = datasetFolderURL
+        
+        for i in 1...maxSamples {
+            let mmmFilename = String(format: "%05d_mmm.xml", i)
+            let annotationsFilename = String(format: "%05d_annotations.json", i)
+            print("\(i), \(mmmFilename), \(annotationsFilename)")
+            
+            let mmmURL = datasetFolderURL.appendingPathComponent(mmmFilename)
+            let annotationsURL = datasetFolderURL.appendingPathComponent(annotationsFilename)
+            
+            let motionSample = MotionSample(mmmURL: mmmURL, annotationsURL: annotationsURL)            
+            motionSamples.append(motionSample)
+        }
     }
-
+    
+    func describe() -> String {
+        return "MotionDataset(motionSamples: \(motionSamples.count))"
+    }
 }
