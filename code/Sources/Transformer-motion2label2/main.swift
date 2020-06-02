0c275e79863d99651aa514001bd5022effabd049
Wojciech Czarnowski <wcz@jatar.com.pl>
2020-06-02 14:01:41 +0000

    Testing batching codes

--- code/Sources/Transformer-motion2label2/main.swift
+++ code/Sources/Transformer-motion2label2/main.swift
@@ -16,10 +16,8 @@ let dataset = try! Motion2Label2(
     batchSize: batchSize
 ) { 
     (example: Motion2LabelExample) -> LabeledMotionBatch in
-    print(1)
     let motionFrames = Tensor<Float>(example.motionSample.motionFramesArray)
     let motionFlag = Tensor<Int32>(motionFrames[0..., 44...44])
-    print(motionFlag.shape)
     let motionBatch = MotionBatch(motionFrames: motionFrames, motionFlag: motionFlag)
     let label = Tensor<Int32>(Int32(example.label!.idx))
     return LabeledMotionBatch(
@@ -31,5 +29,18 @@ let dataset = try! Motion2Label2(
 print("dataset.trainingExamples.count: \(dataset.trainingExamples.count)")
 print("dataset.validationExamples.count: \(dataset.validationExamples.count)")
 
-print("dataset.trainingExamples[0]: \(dataset.trainingExamples[0])")
+// print("dataset.trainingExamples[0]: \(dataset.trainingExamples[0])")
+
+for (epoch, epochBatches) in dataset.trainingEpochs.prefix(5).enumerated() {
+    print("[Epoch \(epoch + 1)]")
+    for batch in epochBatches {
+        // print(7)
+    }
+}
+
+print("dataset.validationBatches.count: \(dataset.validationBatches.count)")
+for batch in dataset.validationBatches {
+    // print(8)
+}
+
 print("Finito.")
