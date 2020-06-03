ed7ab3ec0d84409c150792e508d515299fb13bba
Wojciech Czarnowski <wcz@jatar.com.pl>
2020-06-03 05:03:55 +0000

    Get one batch and print stats
    Processed small dataset (100 samples)

--- code/Sources/PreprocessMotionData/main.swift
+++ code/Sources/PreprocessMotionData/main.swift
@@ -1,17 +1,22 @@
 import Foundation
 import Datasets
 
-let maxSamples: Int = 500
+let maxSamples: Int = 100
+let grouppedJoints = false
+let normalized = true
+
 let datasetFolderURL = URL(fileURLWithPath: "/notebooks/language2motion.gt/data/2017-06-22/")
-let serializedDatasetURL = URL(fileURLWithPath: "/notebooks/language2motion.gt/data/motion_dataset.motion_flag.grouppedJoints.normalized.\(maxSamples).plist")
+let grouppedJointsStr = grouppedJoints ? "grouppedJoints." : ""
+let normalizedStr = normalized ? "normalized." : ""
+let serializedDatasetURL = URL(fileURLWithPath: "/notebooks/language2motion.gt/data/motion_dataset.motion_flag.\(grouppedJointsStr)\(normalizedStr)\(maxSamples).plist")
 
 print("Running MotionData preprocessing (maxSamples=\(maxSamples))...")
 
 var date = Date()
-let motionData = MotionData(datasetFolderURL: datasetFolderURL, maxSamples: maxSamples, grouppedJoints: true, normalized: true)
+let motionData = MotionData(datasetFolderURL: datasetFolderURL, maxSamples: maxSamples, grouppedJoints: grouppedJoints, normalized: true)
 print(abs(date.timeIntervalSinceNow))
 
 date = Date()
-print("Encoding to property list..., writing to file \(serializedDatasetURL)")
+print("Encoding to property list..., writing to file '\(serializedDatasetURL.path)'")
 motionData.write(to: serializedDatasetURL)
 print("Done in \(abs(date.timeIntervalSinceNow)) sec.")
--- code/Sources/Transformer-motion2label2/main.swift
+++ code/Sources/Transformer-motion2label2/main.swift
@@ -6,15 +6,20 @@ import Datasets
 let batchSize = 2
 let tensorWidth = 60
 
-let serializedDatasetURL = URL(fileURLWithPath: "/notebooks/language2motion.gt/data/motion_dataset.motion_flag.normalized.500.plist")
+print("batchSize: \(batchSize)")
+print("tensorWidth: \(tensorWidth)")
+
+let serializedDatasetURL = URL(fileURLWithPath: "/notebooks/language2motion.gt/data/motion_dataset.motion_flag.normalized.100.plist")
 let labelsURL = URL(fileURLWithPath: "/notebooks/language2motion.gt/data/labels_ds_v2.csv")
 
+print("\nLoading dataset...")
 let dataset = try! Motion2Label2(
     serializedDatasetURL: serializedDatasetURL,
     labelsURL: labelsURL,
     maxSequenceLength: tensorWidth,
     batchSize: batchSize
 ) { 
+    // TODO: move this to the dataset
     (example: Motion2LabelExample) -> LabeledMotionBatch in
     let motionFrames = Tensor<Float>(example.motionSample.motionFramesArray)
     let motionFlag = Tensor<Int32>(motionFrames[0..., 44...44])
@@ -31,16 +36,29 @@ print("dataset.validationExamples.count: \(dataset.validationExamples.count)")
 
 // print("dataset.trainingExamples[0]: \(dataset.trainingExamples[0])")
 
-for (epoch, epochBatches) in dataset.trainingEpochs.prefix(5).enumerated() {
-    print("[Epoch \(epoch + 1)]")
-    for _ in epochBatches {
-        // print(7)
-    }
-}
+// for (epoch, epochBatches) in dataset.trainingEpochs.prefix(5).enumerated() {
+//     print("[Epoch \(epoch + 1)]")
+//     for _ in epochBatches {
+//         // print(7)
+//     }
+// }
 
-print("dataset.validationBatches.count: \(dataset.validationBatches.count)")
-for _ in dataset.validationBatches {
-    // print(8)
-}
+// print("dataset.validationBatches.count: \(dataset.validationBatches.count)")
+// for _ in dataset.validationBatches {
+//     // print(8)
+// }
 
-print("Finito.")
+// get a batch
+print("\nOne batch (LabeledMotionBatch):")
+var epochIterator = dataset.trainingEpochs.enumerated().makeIterator()
+let epoch = epochIterator.next()
+let batches = Array(epoch!.1)
+let batch = batches[0]
+print("type: \(type(of:batch))")
+print("\nOne motionBatch")
+let motionBatch = batch.data
+print("type: \(type(of:motionBatch))")
+print("motionFrames.shape: \(motionBatch.motionFrames.shape)")
+print("motionFlag.shape: \(motionBatch.motionFlag.shape)")
+
+print("\nFinito.")
