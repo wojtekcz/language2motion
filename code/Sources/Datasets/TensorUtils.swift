7f2bbb19f4c80a9122819aa108027ab1326a5435
Wojciech Czarnowski <wcz@jatar.com.pl>
2020-06-03 14:51:16 +0000

    paddedOrCropped() works with one-dimensional tensor, too

--- code/Sources/Datasets/TensorUtils.swift
+++ code/Sources/Datasets/TensorUtils.swift
@@ -2,14 +2,16 @@ import TensorFlow
 
 extension Tensor where Scalar: Numeric {
     func paddedOrCropped(to width: Int) -> Tensor<Scalar> {
-        // pads or crops two-dimensional tensor along 0-th axis
-        assert(self.shape.count == 2)
+        // pads or crops one- or two-dimensional tensor along 0-th axis
+        let rank = self.shape.count
         let currentWidth = self.shape[0]
         let paddingSize = Swift.max(width - currentWidth, 0)
         let maxCropping = Swift.max(currentWidth - width, 0)
         let nCropping = (maxCropping>0) ? Int.random(in: 0 ..< maxCropping) : 0
-        return self[nCropping..<nCropping+width].padded(forSizes: [
-            (before: 0, after: paddingSize), 
-            (before: 0, after: 0)])
+        var sizes: [(before: Int, after: Int)] = [(before: 0, after: paddingSize)]
+        if rank > 1 {
+            sizes.append((before: 0, after: 0))
+        }
+        return self[nCropping..<nCropping+width].padded(forSizes: sizes)
     }
 }
