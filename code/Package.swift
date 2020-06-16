8df4edc111c369ac46f9dda5d803ec255c25d550
Wojciech Czarnowski <wcz@jatar.com.pl>
2020-06-16 13:19:10 +0200

    Killed obsolete targets

--- code/Package.swift
+++ code/Package.swift
@@ -17,11 +17,8 @@ let package = Package(
         .library(name: "MotionModels", targets: ["MotionModels"]),
         .library(name: "SummaryWriter", targets: ["SummaryWriter"]),
         .executable(name: "PreprocessMotionData", targets: ["PreprocessMotionData"]),
-        .executable(name: "ResNet-img2label", targets: ["ResNet-img2label"]),
-        .executable(name: "ResNet-motion2label", targets: ["ResNet-motion2label"]),
         .executable(name: "BERT-language2label", targets: ["BERT-language2label"]),
         .executable(name: "Transformer-motion2label2", targets: ["Transformer-motion2label2"]),
-        .executable(name: "Transformer2-motion2label2", targets: ["Transformer2-motion2label2"])
     ],
     dependencies: [
         .package(name: "SwiftProtobuf", url: "https://github.com/apple/swift-protobuf.git", from: "1.9.0")
@@ -30,20 +27,11 @@ let package = Package(
         .testTarget(name: "MotionDatasetTests", dependencies: ["Datasets"]),
         .target(name: "PreprocessMotionData", dependencies: ["Datasets"], path: "Sources/PreprocessMotionData"),
         .target(
-            name: "ResNet-img2label", dependencies: ["ImageClassificationModels", "Datasets"],
-            path: "Sources/ResNet-img2label"),
-        .target(
-            name: "ResNet-motion2label", dependencies: ["ImageClassificationModels", "Datasets", "SummaryWriter"],
-            path: "Sources/ResNet-motion2label"),
-        .target(
             name: "BERT-language2label", dependencies: ["TextModels", "Datasets", "SummaryWriter"],
             path: "Sources/BERT-language2label"),
         .target(
             name: "Transformer-motion2label2", dependencies: ["ImageClassificationModels", "TextModels", "Datasets", "ModelSupport", "MotionModels", "SummaryWriter"],
             path: "Sources/Transformer-motion2label2"),
-        .target(
-            name: "Transformer2-motion2label2", dependencies: ["ImageClassificationModels", "TextModels", "Datasets", "ModelSupport", "MotionModels", "SummaryWriter"],
-            path: "Sources/Transformer2-motion2label2"),
         .target(name: "Batcher", path: "Sources/Batcher"),
         .target(name: "Datasets", dependencies: ["ModelSupport", "Batcher"], path: "Sources/Datasets"),
         .target(name: "ImageClassificationModels", path: "Sources/Models/ImageClassification"),
