8d4938de68bfebbaac06ddf94eb5d308eb46ee93
Wojciech Czarnowski <wcz@jatar.com.pl>
2020-06-23 20:01:23 +0200

    Copying tensors to CPU to speed up accessing scalars

--- code/Sources/Models/Motion/MotionClassifierProtocol.swift
+++ code/Sources/Models/Motion/MotionClassifierProtocol.swift
@@ -36,24 +36,21 @@ extension MotionClassifierProtocol {
 
         var preds: [Prediction] = []
         for eagerBatch in validationBatches {
-            print(1)
             let batch = MotionBatch(copying: eagerBatch, to: device)
             let logits = self(batch)
             let probs = softmax(logits, alongAxis: 1)
             let classIdxs = logits.argmax(squeezingAxis: 1)
             LazyTensorBarrier()
-            // TODO: how to copy it to CPU?
-            print(2)
-            let eagerClassIdxs = Tensor(copying: classIdxs, to: Device.defaultTFEager)            
-            print(3)
+            // copy tensors to CPU
+            let eagerClassIdxs = Tensor(copying: classIdxs, to: Device.defaultTFEager)
+            let eagerProbs = Tensor(copying: probs, to: Device.defaultTFEager)
             let batchPreds = (0..<eagerClassIdxs.shape[0]).map { 
                 (idx) -> Prediction in
                 let classIdx: Int = Int(eagerClassIdxs[idx].scalar!)
-                let prob = probs[idx, classIdx].scalar!
+                let prob = eagerProbs[idx, classIdx].scalar!
                 return Prediction(classIdx: classIdx, className: labels[classIdx], probability: prob)
             }
             preds.append(contentsOf: batchPreds)
-            print(4)
         }
         return preds
     }
