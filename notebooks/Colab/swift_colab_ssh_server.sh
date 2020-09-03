fb62c0d42e70772ee4f39d5d56c85e0e29627276
Wojciech Czarnowski <wcz@jatar.com.pl>
2020-09-03 09:40:22 +0000

    Updated

--- notebooks/Colab/swift_colab_ssh_server.sh
+++ notebooks/Colab/swift_colab_ssh_server.sh
@@ -5,24 +5,24 @@ pip2 install glances google-auth-oauthlib==0.4.1 grpcio==1.24.3
 baseURL=https://raw.githubusercontent.com/wojtekcz/language2motion/master/notebooks/Colab/
 
 # cp sshd_config /etc/ssh/sshd_config
-wget $baseURL/sshd_config -O /etc/ssh/sshd_config
+wget -nv $baseURL/sshd_config -O /etc/ssh/sshd_config
 
 /etc/init.d/ssh restart
 /etc/init.d/ssh status
 
 # cp bashrc /root/.bashrc
-wget $baseURL/bashrc -O /root/.bashrc
+wget -nv $baseURL/bashrc -O /root/.bashrc
 
 mkdir -p /root/.ssh
 chmod 700 /root/.ssh
 
 # cp authorized_keys /root/.ssh/authorized_keys
-wget $baseURL/authorized_keys -O /root/.ssh/authorized_keys
+wget -nv $baseURL/authorized_keys -O /root/.ssh/authorized_keys
 chmod 600 /root/.ssh/authorized_keys
 
 privateKeyPath=/root/.ssh/private_key.pem
 # cp private_key.pem $privateKeyPath
-wget $baseURL/private_key.pem -O $privateKeyPath
+wget -nv $baseURL/private_key.pem -O $privateKeyPath
 chmod 600 $privateKeyPath
 
 options="-oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null"
