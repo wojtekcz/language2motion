dd0eda4f71dffb758d5ae1d6660343df304c45db
Wojciech Czarnowski <wcz@jatar.com.pl>
2020-09-16 10:02:14 +0200

    Tests won't build anymore

--- Tests/MotionDatasetTests/MotionFrameTests.swift
+++ Tests/MotionDatasetTests/MotionFrameTests.swift
@@ -9,77 +9,77 @@ final class MotionDatasetTests: XCTestCase {
         return URL(fileURLWithPath: "/notebooks/language2motion.gt/data/motion_dataset.grouppedJoints.normalized.\(maxSamples).plist")
     }
 
-    public var description: String {
+    public override var description: String {
         return "MotionDataset(motionSamples: \(maxSamples))"
     }
 
     func serializeMotionSample() throws {
-        let mmmURL = URL(fileURLWithPath: "/notebooks/language2motion.gt/data/2017-06-22/00186_mmm.xml")
-        let annotationsURL = URL(fileURLWithPath: "/notebooks/language2motion.gt/data/2017-06-22/00186_annotations.json")
-        let m186 = MotionSample(sampleID: 186, mmmURL: mmmURL, annotationsURL: annotationsURL)
-        print(m186.description)
-        print(m186.annotations)
-        // serialize sample to JSON
-        let encoder = JSONEncoder()
-        encoder.outputFormatting = .prettyPrinted
-
-        let msJSON = try encoder.encode(m186)
-        print(String(data: msJSON, encoding: .utf8)!)
-
-        // decode from JSON
-        let decoder = JSONDecoder()
-        let ms2 = try decoder.decode(MotionSample.self, from: msJSON)
-        print(ms2)
+//        let mmmURL = URL(fileURLWithPath: "/notebooks/language2motion.gt/data/2017-06-22/00186_mmm.xml")
+//        let annotationsURL = URL(fileURLWithPath: "/notebooks/language2motion.gt/data/2017-06-22/00186_annotations.json")
+//        let m186 = MotionSample(sampleID: 186, mmmURL: mmmURL, annotationsURL: annotationsURL)
+//        print(m186.description)
+//        print(m186.annotations)
+//        // serialize sample to JSON
+//        let encoder = JSONEncoder()
+//        encoder.outputFormatting = .prettyPrinted
+//
+//        let msJSON = try encoder.encode(m186)
+//        print(String(data: msJSON, encoding: .utf8)!)
+//
+//        // decode from JSON
+//        let decoder = JSONDecoder()
+//        let ms2 = try decoder.decode(MotionSample.self, from: msJSON)
+//        print(ms2)
     }
 
     func serializeMotionDataJSON() throws {
-        let datasetFolderURL = URL(fileURLWithPath: "/notebooks/language2motion.gt/data/2017-06-22/")
-
-        var date = Date()
-        let motionData = MotionData(datasetFolderURL: datasetFolderURL, maxSamples: 10)
-        print(abs(date.timeIntervalSinceNow))
-
-        // serialize dataset to JSON
-        let encoder = JSONEncoder()
-        encoder.outputFormatting = .prettyPrinted
-
-        let mdJSON = try encoder.encode(motionData)
-        let jsonStr = String(data: mdJSON, encoding: .utf8)!
-        // print(jsonStr)
-        print(jsonStr.count)
-        let fileURL = URL(fileURLWithPath: "/notebooks/language2motion.gt/data/dataset.json")
-        try jsonStr.write(to: fileURL, atomically: true, encoding: String.Encoding.utf8)
-        // try JSONSerialization.data(withJSONObject: mdJSON)
-        // .write(to: fileURL)
-
-        // decode from JSON
-        print("Decoding...")
-        let decoder = JSONDecoder()
-        date = Date()
-        let md2 = try decoder.decode(MotionData.self, from: mdJSON)
-        print(md2.description)
-        print(abs(date.timeIntervalSinceNow))
+//        let datasetFolderURL = URL(fileURLWithPath: "/notebooks/language2motion.gt/data/2017-06-22/")
+//
+//        var date = Date()
+//        let motionData = MotionData(datasetFolderURL: datasetFolderURL, maxSamples: 10)
+//        print(abs(date.timeIntervalSinceNow))
+//
+//        // serialize dataset to JSON
+//        let encoder = JSONEncoder()
+//        encoder.outputFormatting = .prettyPrinted
+//
+//        let mdJSON = try encoder.encode(motionData)
+//        let jsonStr = String(data: mdJSON, encoding: .utf8)!
+//        // print(jsonStr)
+//        print(jsonStr.count)
+//        let fileURL = URL(fileURLWithPath: "/notebooks/language2motion.gt/data/dataset.json")
+//        try jsonStr.write(to: fileURL, atomically: true, encoding: String.Encoding.utf8)
+//        // try JSONSerialization.data(withJSONObject: mdJSON)
+//        // .write(to: fileURL)
+//
+//        // decode from JSON
+//        print("Decoding...")
+//        let decoder = JSONDecoder()
+//        date = Date()
+//        let md2 = try decoder.decode(MotionData.self, from: mdJSON)
+//        print(md2.description)
+//        print(abs(date.timeIntervalSinceNow))
     }
 
     func serializeMotionDataBinary() throws {
-        let datasetFolderURL = URL(fileURLWithPath: "/notebooks/language2motion.gt/data/2017-06-22/")
-
-        var date = Date()
-        let motionData = MotionData(datasetFolderURL: datasetFolderURL, maxSamples: maxSamples, grouppedJoints: true, normalized: true)
-        print(abs(date.timeIntervalSinceNow))
-
-        date = Date()
-        print("Encoding to property list..., writing to file...")
-        motionData.write(to: serializedDatasetURL)
-        print("Done in \(abs(date.timeIntervalSinceNow)) sec.")
+//        let datasetFolderURL = URL(fileURLWithPath: "/notebooks/language2motion.gt/data/2017-06-22/")
+//
+//        var date = Date()
+//        let motionData = MotionData(datasetFolderURL: datasetFolderURL, maxSamples: maxSamples, grouppedJoints: true, normalized: true)
+//        print(abs(date.timeIntervalSinceNow))
+//
+//        date = Date()
+//        print("Encoding to property list..., writing to file...")
+//        motionData.write(to: serializedDatasetURL)
+//        print("Done in \(abs(date.timeIntervalSinceNow)) sec.")
     }
 
     func readBinaryMotionData() throws {
-        print("Reading..., decoding...")
-        let date = Date() 
-        let motionData = MotionData(from: serializedDatasetURL)
-        print("Done in \(abs(date.timeIntervalSinceNow)) sec.")
-        print(motionData.description)
+//        print("Reading..., decoding...")
+//        let date = Date() 
+//        let motionData = MotionData(from: serializedDatasetURL)
+//        print("Done in \(abs(date.timeIntervalSinceNow)) sec.")
+//        print(motionData.description)
     }
 
     static var allTests = [
