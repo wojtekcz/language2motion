f343e607987caaf7e31540b62f6f5a2a6b74cd8d
Wojtek Czarnowski <wojtek.czarnowski@gmail.com>
2020-10-08 10:37:51 +0000

    Testing forward pass on X10

--- Tests/MotionDatasetTests/LangMotionTransformerTests.swift
+++ Tests/MotionDatasetTests/LangMotionTransformerTests.swift
@@ -155,8 +155,8 @@ class LangMotionTransformerTests: XCTestCase {
         let _ = _ExecutionContext.global
 
         /// Select eager or X10 backend
-        // let device = Device.defaultXLA
-        let device = Device.defaultTFEager
+        let device = Device.defaultXLA
+        // let device = Device.defaultTFEager
         print("backend: \(device)")
 
         textProcessor = getTextProcessor()
@@ -173,44 +173,8 @@ class LangMotionTransformerTests: XCTestCase {
 
         print("\n===> start test")
         model.move(to: device)
-        // let _ = model(source)
-        let input = source
         time {
-            print(1)
-            let encoded = model.encode(input: input.sentence)
-            LazyTensorBarrier()
-
-            print(2)
-            let decoded = model.decode(sourceMask: input.sourceAttentionMask, motionPart: input.motionPart, memory: encoded.lastLayerOutput)
-            LazyTensorBarrier()
-
-            time {
-                print(3)
-                Context.local.learningPhase = .inference
-                let mixtureModelInput = decoded.lastLayerOutput
-                let predsO = model.mixtureModel.callAsFunction_old(mixtureModelInput)
-                let predsN = model.mixtureModel(mixtureModelInput)
-                LazyTensorBarrier()
-
-                // func roundT(_ num: Tensor<Float>, prec: Int = 4) -> Tensor<Float> {
-                //     return round(num*1e4)/1e4
-                // }
-
-                // TODO: compare mixture model old and new outputs
-                print("mixtureMeans ", (predsO.mixtureMeans - predsN.mixtureMeans).sum().scalar! < 1e-3)
-                print("mixtureVars:", (predsO.mixtureVars - predsN.mixtureVars).sum().scalar! < 1e-3)
-                print("mixtureWeights:", (predsO.mixtureWeights - predsN.mixtureWeights).sum().scalar! < 1e-3)
-                print("stops:", predsO.stops == predsN.stops)
-
-                // predsO.printPreds()
-                // print("predsO")
-                // print(roundT(predsO.mixtureMeans[0, 0...1]))
-                // print("predsN")
-                // // predsN.printPreds()
-                // print(roundT(predsN.mixtureMeans[0, 0...1]))
-            }
-            print(4)
-            // let rslt = LangMotionTransformerOutput(preds: preds, encoded: encoded, decoded: decoded)
+            let _ = model(source)
         }
         print("===> end test\n")
     }
