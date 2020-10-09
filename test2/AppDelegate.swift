0ff5805c2b334e1d6ec64c60dabfc92f59032c9a
Wojciech Czarnowski <wcz@jatar.com.pl>
2020-10-09 09:00:16 +0200

    Motion generation UI scaffolding

---
+++ test2/AppDelegate.swift
@@ -0,0 +1,42 @@
+//
+//  AppDelegate.swift
+//  test2
+//
+//  Created by Wojciech Czarnowski on 10/9/20.
+//
+
+import Cocoa
+import SwiftUI
+import TensorFlow
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Create the SwiftUI view that provides the window contents.
+        let contentView = ContentView()
+
+        // Create the window and set the content view.
+        window = NSWindow(
+            contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
+            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
+            backing: .buffered, defer: false)
+        window.isReleasedWhenClosed = false
+        window.center()
+        window.setFrameAutosaveName("Main Window")
+        window.contentView = NSHostingView(rootView: contentView)
+        window.makeKeyAndOrderFront(nil)
+        
+        let t1 = Tensor<Float>([1.0, 2.0, 3.0])
+        print(t1 .* t1)
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
