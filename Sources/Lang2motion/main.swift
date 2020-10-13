37d663a99f10f10c249841ccc2f0780518d52cf3
Wojtek Czarnowski <wojtek.czarnowski@gmail.com>
2020-10-13 13:21:02 +0000

    run_85 on colab

--- Sources/Lang2motion/main.swift
+++ Sources/Lang2motion/main.swift
@@ -13,7 +13,7 @@ import TrainingLoop
 import x10_optimizers_optimizer
 
 /// Set training params
-let runName = "run_84"
+let runName = "run_85"
 let batchSize = 50
 let maxTextSequenceLength =  40
 let maxMotionLength =  300
@@ -21,7 +21,7 @@ let nEpochs = 30
 
 // peek LR for new training: 1e-3, for resuming: 5e-4
 var optimizerOpts = OptimizerOpts(
-    peakLearningRate: 2e-4,
+    peakLearningRate: 5e-4,
     beta1: 0.9,
     beta2: 0.999,
     useBiasCorrection: false,
