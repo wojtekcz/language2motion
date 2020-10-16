f2765b5a52a64b510791cb03d6d76147847589b7
Wojtek Czarnowski <wojtek.czarnowski@gmail.com>
2020-10-16 14:40:46 +0000

    run_set_17

--- Sources/Lang2motionSet/main.swift
+++ Sources/Lang2motionSet/main.swift
@@ -13,7 +13,7 @@ import TrainingLoop
 import x10_optimizers_optimizer
 
 /// Set training params
-let runSetName = "run_set_16"
+let runSetName = "run_set_17"
 let batchSize = 2
 let maxTextSequenceLength =  40
 let maxMotionLength =  50
@@ -23,15 +23,18 @@ let datasetSize: DatasetSize = .small_micro
 let multiplyFactor = 50
 
 let commonRunsSettings: [String:Any] = [
-    "dropout": 0.00001, "wd": 0.0001, "beta2": 0.9999
+    "lr": 1e-6, "dropout": 0.00001, "wd": 0.0001, "beta2": 0.9999
 ]
 
 // peek LR for new training: 1e-3, for resuming: 5e-4 (for full dataset)
 let runsSettings: [[String:Any]] = [
-    ["lr": 1e-6],
-    ["lr": 2e-6, "dropout": 0.00001],
-    ["lr": 2e-6, "wd": 0.00001],
-    ["lr": 2e-6, "beta2": 0.999],
+   ["dropout": 0.1],
+   ["dropout": 0.01],
+   ["dropout": 0.001],
+   ["dropout": 0.0001],
+   ["dropout": 0.00001],
+   ["dropout": 0.000001],
+   ["dropout": 0.0],
 ]
 
 //print("runName: \(runName)")
