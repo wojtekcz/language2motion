bcd508e843eee0106adc4760410c267996d9137e
Wojtek Czarnowski <wojtek.czarnowski@gmail.com>
2020-10-27 14:56:34 +0000

    run_113

--- Sources/Lang2motion/main.swift
+++ Sources/Lang2motion/main.swift
@@ -13,8 +13,8 @@ import TrainingLoop
 import x10_optimizers_optimizer
 
 /// Set training params
-let runName = "run_112"
-let batchSize = 2
+let runName = "run_113"
+let batchSize = 93
 let maxTextSequenceLength =  40
 let maxMotionLength =  50
 let nEpochs = 100
@@ -40,7 +40,7 @@ var optimizerOpts = OptimizerOpts(
     fixedPeekLR: fixedPeekLR
 )
 
-let datasetSize: DatasetSize = .small_micro
+let datasetSize: DatasetSize = .small_midi
 
 print("runName: \(runName)")
 print("batchSize: \(batchSize)")
