f488f32c63c5e3f6f9db6c0e1ce713ca71d0b65f
Wojtek Czarnowski <wcz@jatar.com.pl>
2020-10-31 14:49:21 +0100

    Scaffolding for categorical distribution based head implementation

---
+++ Tests/MotionDatasetTests/CategoricalDistributionHeadTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+import Foundation
+import TensorFlow
+
+class CategoricalDistributionHeadTests: XCTestCase {
+
+    func testExample() throws {
+        print("Ala ma kota")
+        print(Tensor<Float>([[1.0]]) + 1)
+    }
+
+}
