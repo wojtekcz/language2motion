31ae27f186f51852208116371b838d28a17a957e
Wojtek Czarnowski <wojtek.czarnowski@gmail.com>
2020-11-10 14:27:04 +0000

    run_166

--- Sources/Lang2motion/main.swift
+++ Sources/Lang2motion/main.swift
@@ -17,7 +17,7 @@ let maxSamples: Int? = 500
 
 let maxSamplesStr = maxSamples != nil ? "_\(maxSamples!)" : ""
 
-let runName = "run_165"//_maxSamples\(maxSamplesStr)"
+let runName = "run_166"//_maxSamples\(maxSamplesStr)"
 let batchSize = 50
 let maxTextSequenceLength =  40
 let maxMotionLength = 75
@@ -26,7 +26,7 @@ let multiplyFactor = 4
 let discreteBins = 300
 let lrSlopeMultiplier: Float = 1.0
 let fixedPeekLR: Bool = true
-let peakLearningRate: Float = 1e-2 //5e-3
+let peakLearningRate: Float = 2e-2 //5e-3
 let useBiasCorrection: Bool = true
 let weightDecayRate: Float = 0.001
 let beta2: Float = 0.99
