95ecfa784d4a057af7c67b19965a2f95a67a02f5
Wojciech Czarnowski <wcz@jatar.com.pl>
2020-11-16 10:23:07 +0100

    Project folder structure cleanups

--- Package.swift
+++ Package.swift
@@ -30,7 +30,7 @@ let package = Package(
         .package(name: "SwiftProtobuf", url: "https://github.com/apple/swift-protobuf.git", from: "1.10.2")
     ],
     targets: [
-        .testTarget(name: "MotionDatasetTests", dependencies: ["Datasets", "ModelSupport", "LangMotionModels", "SummaryWriter"]),
+        .testTarget(name: "Language2MotionTests", dependencies: ["Datasets", "ModelSupport", "LangMotionModels", "SummaryWriter"]),
         .target(
             name: "Checkpoints", dependencies: ["SwiftProtobuf", "ModelSupport"],
             path: "Sources/Other/Checkpoints"),
