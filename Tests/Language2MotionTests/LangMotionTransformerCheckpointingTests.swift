814b9c6e671fc4411a27a8426de4aae330f4b3db
Wojciech Czarnowski <wcz@jatar.com.pl>
2020-11-16 16:13:59 +0100

    Scaffolding for model checkpointing improvements

--- Tests/Language2MotionTests/LangMotionTransformerCheckpointingTests.swift
+++ Tests/Language2MotionTests/LangMotionTransformerCheckpointingTests.swift
@@ -46,4 +46,44 @@ class LangMotionTransformerCheckpointingTests: XCTestCase {
         let config = LangMotionCatDistTransformerConfig.createFromJSONURL(configURL)!
         print(config)
     }
+    
+    func getCheckpointURL() -> URL {
+        return dataURL.appendingPathComponent("runs/Lang2motion").appendingPathComponent("run_181").appendingPathComponent("checkpoints")
+    }
+    
+    func testTransformerModelWriting() throws {
+        
+        let vocabSize = 1000
+
+        let config = LangMotionCatDistTransformerConfig(
+            vocabSize: vocabSize,
+            nbJoints: 47,
+            layerCount: 4,
+            encoderDepth: 64,
+            decoderDepth: 240,
+            feedForwardSize: 256,
+            headCount: 4,
+            dropoutProbability: 0.1,
+            sentenceMaxPositionalLength: 100,
+            motionMaxPositionalLength: 500,
+            discreteBins: 300,
+            activation: .swish
+        )
+
+        let model = LangMotionCatDistTransformer(config: config)
+        
+        let checkpointURL = getCheckpointURL()
+        
+        try! FileManager().createDirectory(at: checkpointURL, withIntermediateDirectories: true)
+        try model.writeCheckpoint(to: checkpointURL, name: "model.t1")
+        try config.write(to: checkpointURL.appendingPathComponent("model_config.json"))
+    }
+
+    func testTransformerModelReading() throws {
+
+        let checkpointURL = getCheckpointURL()
+        let config = LangMotionCatDistTransformerConfig.createFromJSONURL(checkpointURL.appendingPathComponent("model_config.json"))!
+        let model = try LangMotionCatDistTransformer(checkpoint: checkpointURL, config: config, name: "model.t1")
+        print(model.config.layerCount)
+    }
 }
